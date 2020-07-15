7c7f9e5e97345baa4c70fdc7f4f870639ac8ee39
Leonardo Oliveira Balsalobre <57500163+oliveirabalsa@users.noreply.github.com>
2020-07-15 14:54:55 -0300

    feat: add laboratory models and usecases

---
+++ src/domain/usecases/add-laboratory.ts
@@ -0,0 +1,11 @@
+import { LaboratoryModel } from './models/laboratory-model'
+
+export interface AddLaboratoryModel {
+  name: string
+  address: string
+  status: boolean
+}
+
+export interface AddLaboratory {
+  add: (account: AddLaboratoryModel) => Promise<LaboratoryModel>
+}
---
+++ src/domain/usecases/models/laboratory-model.ts
@@ -0,0 +1,6 @@
+export interface LaboratoryModel {
+  id: string
+  name: string
+  address: string
+  status: boolean
+}
