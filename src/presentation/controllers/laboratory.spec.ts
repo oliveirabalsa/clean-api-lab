558234950313ebbb7bada759188dc25770b362fe
Leonardo Oliveira Balsalobre <57500163+oliveirabalsa@users.noreply.github.com>
2020-07-15 00:15:25 -0300

    test: ensure LaboratoryController returns 400 if no name is provided

---
+++ src/presentation/controllers/laboratory.spec.ts
@@ -0,0 +1,14 @@
+import { LaboratoryController } from './laboratoy'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new LaboratoryController()
+    const httpRequest = {
+      address: 'any_address',
+      status: true
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
