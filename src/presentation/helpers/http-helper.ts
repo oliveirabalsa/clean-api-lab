2968c67c50d1b1d7265b26d9c97a796e2db1dab1
Leonardo Oliveira Balsalobre <57500163+oliveirabalsa@users.noreply.github.com>
2020-07-15 00:58:02 -0300

    feat: add http helper

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,17 @@
+import { HttpResponse } from '../protocols/http'
+import { ServerError } from '../errors/server-error'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
