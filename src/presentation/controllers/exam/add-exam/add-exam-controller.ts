9ae52291c1db28177dd1196dd0f579c436df35e5
Leonardo Oliveira Balsalobre <57500163+oliveirabalsa@users.noreply.github.com>
2020-07-17 10:57:03 -0300

    feat: add endpint create exam

--- src/presentation/controllers/exam/add-exam/add-exam-controller.ts
+++ src/presentation/controllers/exam/add-exam/add-exam-controller.ts
@@ -1,27 +1,27 @@
 import { response } from '../../../helpers/response-helper'
 import { serverError, badRequest, ok } from '../../../helpers/http-helper'
 import { MissingParamError } from '../../../errors'
-import { LaboratoryService } from '../../../services/laboratory/laborabory-service'
+import { ExamService } from '../../../services/exam/exam-service'
 import { Request, Response } from 'express'
 
-export class LaboratoryController {
+export class ExamController {
   async handle (req: Request, res: Response): Promise<any> {
     try {
-      const laboratoryService = new LaboratoryService()
-      const requiredFields = ['name', 'address', 'status']
+      const examService = new ExamService()
+      const requiredFields = ['name', 'type', 'status']
       for (const field of requiredFields) {
         if (!req.body[field]) {
           return response(badRequest(new MissingParamError(field)))
         }
       }
-      const { name, address, status } = req.body
-      const laboratory = await laboratoryService.save({
+      const { name, type, status } = req.body
+      const exam = await examService.save({
         name,
-        address,
+        type,
         status
       })
 
-      await res.status(200).json(ok(laboratory))
+      await res.status(200).json(ok(exam))
     } catch (error) {
       // console.log(error.message)
       return response(serverError())
--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -6,15 +6,25 @@ import {
   UpdateLaboratoryController
 } from './controllers/laboratory/index'
 
+import { ExamController } from './controllers/exam/add-exam/add-exam-controller'
+
 const routes = express()
+
 const laboratoryController = new LaboratoryController()
 const loadLaboratoryController = new LoadLaboratoryController()
 const deleteLaboratoryController = new DeleteLaboratoryController()
 const updateLaboratoryController = new UpdateLaboratoryController()
 
+const examController = new ExamController()
+
 routes.post('/laboratory', laboratoryController.handle)
 routes.put('/laboratory/:id', updateLaboratoryController.handle)
 routes.get('/laboratory', loadLaboratoryController.handle)
 routes.delete('/laboratory/:id', deleteLaboratoryController.handle)
+
+routes.post('/exam', examController.handle)
+// routes.put('/laboratory/:id', updateLaboratoryController.handle)
+// routes.get('/laboratory', loadLaboratoryController.handle)
+// routes.delete('/laboratory/:id', deleteLaboratoryController.handle)
 
 export default routes
