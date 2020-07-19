7dc9947024eb5a1b72ff5d291ca29f3efd85f224
Leonardo Oliveira Balsalobre <57500163+oliveirabalsa@users.noreply.github.com>
2020-07-19 20:55:41 -0300

    test: ensure deleteExameController returns 404 if no id is provided

--- src/presentation/controllers/exam/delete-exam/delete.exam.spec.ts
+++ src/presentation/controllers/exam/delete-exam/delete.exam.spec.ts
@@ -4,7 +4,7 @@ import request from 'supertest'
 describe('DeleteLaboratoryController', () => {
   test('Should return 404 if no id is provided', async () => {
     const httpResponse = await request(app)
-      .delete('/laboratory')
+      .delete('/exam')
     const response = httpResponse.body
     expect(httpResponse.statusCode).toBe(404)
     expect(response.body).toEqual('Invalid method or endpoint')
